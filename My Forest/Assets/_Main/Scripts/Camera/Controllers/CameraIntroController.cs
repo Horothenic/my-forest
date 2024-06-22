e2a54f221e424b19b35625d93d69130ffc426759
Ivan Herrera <ivan.herrera.m@outlook.com>
2024-06-22 13:51:02 -0600

    Adjusted intro

--- My Forest/Assets/_Main/Scripts/Camera/Controllers/CameraIntroController.cs
+++ My Forest/Assets/_Main/Scripts/Camera/Controllers/CameraIntroController.cs
@@ -12,9 +12,6 @@ namespace MyForest
         [Inject] private ICameraIntroSource _cameraIntroSource = null;
         [Inject] private ICameraGesturesControlSource _cameraGesturesControlSource = null;
         [Inject] private ICameraGesturesDataSource _cameraGesturesDataSource = null;
-
-        [Header("COMPONENTS")]
-        [SerializeField] private Camera _camera = null;
         
         [Header("UI")]
         [SerializeField] private GameObject _container = null;
@@ -47,21 +44,23 @@ namespace MyForest
         private void FirstIntro()
         {
             var sequence = DOTween.Sequence();
-            sequence.AppendInterval(0.4f);
+            sequence.AppendCallback(_cameraGesturesControlSource.BlockInput);
+            sequence.AppendInterval(0.3f);
             sequence.AppendCallback(_cameraIntroSource.IntroStarted);
             sequence.AppendCallback(() => _cameraGesturesDataSource.SetZoom(40));
-            sequence.Insert(0.2f, _blocker.DOFade(0, 0.45f));
+            sequence.Append(_blocker.DOFade(0, 0.45f));
             sequence.AppendCallback(() => _container.TurnOff());
             sequence.AppendInterval(0.3f);
+            sequence.Append(DOTween.To(() => _cameraGesturesDataSource.CurrentZoom, zoom => _cameraGesturesDataSource.SetZoom(zoom), 10, 2.4f).SetEase(Ease.OutQuart));
             sequence.AppendCallback(_cameraIntroSource.IntroEnded);
             sequence.AppendCallback(_cameraIntroSource.FirstIntroPlayed);
-            sequence.Append(DOTween.To(() => _camera.orthographicSize, zoom => _cameraGesturesDataSource.SetZoom(zoom), 7, 2.4f).SetEase(Ease.InQuad));
             sequence.AppendCallback(_cameraGesturesControlSource.EnableInput);
         }
 
         private void NormalIntro()
         {
-            var sequence = DOTween.Sequence();;
+            var sequence = DOTween.Sequence();
+            sequence.AppendCallback(_cameraGesturesControlSource.BlockInput);
             sequence.AppendCallback(_cameraIntroSource.IntroStarted);
             sequence.Append(_blocker.DOFade(0, 0.3f));
             sequence.AppendCallback(() => _container.TurnOff());
